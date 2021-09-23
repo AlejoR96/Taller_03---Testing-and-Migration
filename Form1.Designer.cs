20be55e9afc69563bfc92a1f05ccc328d2f9629b
Alejandro Rivera <jarivera636@misena.edu.co>
2021-09-23 10:53:56 -0500

    Funcion realizada para calcular horas trabajadas y horas extras

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Taller_Testing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtHoras = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.cbCategoria = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre Empleado\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero Horas";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(160, 41);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(135, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtHoras
+            // 
+            this.txtHoras.Location = new System.Drawing.Point(160, 82);
+            this.txtHoras.Name = "txtHoras";
+            this.txtHoras.Size = new System.Drawing.Size(72, 20);
+            this.txtHoras.TabIndex = 3;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(286, 254);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.TabIndex = 4;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // cbCategoria
+            // 
+            this.cbCategoria.FormattingEnabled = true;
+            this.cbCategoria.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.cbCategoria.Location = new System.Drawing.Point(160, 130);
+            this.cbCategoria.Name = "cbCategoria";
+            this.cbCategoria.Size = new System.Drawing.Size(72, 21);
+            this.cbCategoria.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Categoria";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Total";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(160, 178);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(72, 20);
+            this.txtTotal.TabIndex = 9;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(180, 254);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 10;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 311);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbCategoria);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txtHoras);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Ingreso Empleados";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtHoras;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.ComboBox cbCategoria;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taller_Testing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int horasNormal, horasExtras, categoria;
+        private double precioNormal, precioExtras, total;
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarCajas();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            Operacion();
+        }
+
+        
+
+        private void Operacion()
+        {
+            categoria = (Convert.ToInt32(cbCategoria.SelectedIndex) + 1);
+
+            if (Convert.ToInt32(txtHoras.Text) <= 40)
+            {
+                horasNormal = Convert.ToInt32(txtHoras.Text);
+                horasExtras = 0;
+
+                switch (categoria){
+
+                    case 1:
+                        precioNormal = 12000 * horasNormal;
+                        precioExtras = 0;
+                        total = precioNormal + precioExtras;
+                        break;
+
+                    case 2:
+                        precioNormal = 17000 * horasNormal;
+                        precioExtras = 0;
+                        total = precioNormal + precioExtras;
+                        break;
+
+                    case 3:
+                        precioNormal = 22000 * horasNormal;
+                        precioExtras = 0;
+                        total = precioNormal + precioExtras;
+                        break;
+
+                }
+            }
+            else
+            {
+                horasExtras = Convert.ToInt32(txtHoras.Text) - 40;
+                horasNormal = Convert.ToInt32(txtHoras.Text) - horasExtras;
+
+                switch (categoria)
+                {
+                    case 1:
+                        precioNormal = 12000 * horasNormal;
+                        precioExtras =(12000 * horasExtras)*1.25;
+                        total = precioNormal + precioExtras;
+                        break;
+
+                    case 2:
+                        precioNormal = 17000 * horasNormal;
+                        precioExtras =(17000 * horasExtras) * 1.25;
+                        total = precioNormal + precioExtras;
+                        break;
+
+                    case 3:
+                        precioNormal = 22000 * horasNormal;
+                        precioExtras =(22000 * horasExtras) * 1.25;
+                        total = precioNormal + precioExtras;
+                        break;
+                }
+            }
+        }
+
+        private void LimpiarCajas()
+        {
+            txtNombre.Clear();
+            txtHoras.Clear();
+            txtTotal.Clear();
+            txtNombre.Focus();
+        }
+    }
+}
